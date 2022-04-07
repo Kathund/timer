2c0f7eaf625d99763e1ba3f969c4e6ea221ba696
kath <55346310+Kathund@users.noreply.github.com>
2022-04-07 11:48:54 +0800

    Updated button
    Updated button to reflect what state that the button is currently in

--- js/run.js
+++ js/run.js
@@ -179,10 +179,22 @@
             }
         }
 
-
-function myFunction() {
+        
+function darkToggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-    element.classList.toggle("time-dark");
- }
-   
+}
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function handleClick() {
+  const initialText = 'Toggle Dark mode';
+
+  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn.innerHTML =
+      '<span>Toggle Light mode</span>';
+  } else {
+    btn.textContent = initialText;
+  }
+});
+        
